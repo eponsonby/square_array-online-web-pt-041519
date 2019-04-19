476691cf54d7315ae41ca9767d8f11c3477a55bb
Erin Ponsonby <erinponsonby@gmail.com>
2019-04-19 11:59:06 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,8 +2,7 @@ require 'pry'
 def square_array(array)
   squared_numbers = []
   array.each do |item|
-    squared_numbers << item**
-    binding.pry
+    squared_numbers << item**2
   end
   squared_numbers
 end
